8a77f475c4ba832f7096e87edba6caa4c7a94976
Daulet <muhanbetkali@gmail.com>
2019-02-21 01:44:12 +0600

    Used fetch and json database
    #Day 3 FETCH/json-server/API/

--- js/script.js
+++ js/script.js
@@ -1,131 +1,153 @@
 //использовали это для быстрого загрузки скриптов.так,ведь?!
 window.addEventListener('DOMContentLoaded', () => {
+	
+    const loadContent = async (url, callback) => {
+        await fetch(url)
+            .then(response => response.json())
+            .then(json => createElement(json));
 
-    //выбирая селекторов, объявляем констант
-    const cartWrapper = document.querySelector('.cart__wrapper'),
-        cart = document.querySelector('.cart'),
-        close = document.querySelector('.cart__close'),
-        open = document.querySelector('#cart'),
-        goodsBtn = document.querySelectorAll('.goods__btn'),
-        products = document.querySelectorAll('.goods__item'),
-        confirm = document.querySelector('.confirm'),
-        badge = document.querySelector('.nav__badge'),
-        totalCost = document.querySelector('.cart__total > span'),
-        titles = document.querySelectorAll('.goods__title');
-
-    // создаем функцию для открытия корзины
-    function openCart() {
-        cart.style.display = 'block';
-        document.body.style.overflow = 'hidden';
-    }
-
-    //это для закрытия корзину :)
-    function closeCart() {
-        cart.style.display = 'none';
-        document.body.style.overflow = '';
-    }
-
-    //чтобы работало функций, назначаем обработчики событий
-    open.addEventListener('click', openCart);
-    close.addEventListener('click', closeCart);
-
-    //создаем массив кнопки "Добавить в корзину" для всех товаров
-    goodsBtn.forEach(function (btn, i) {
-        btn.addEventListener('click', () => {
-            let item = products[i].cloneNode(true),
-                trigger = item.querySelector('button'),
-                removeBtn = document.createElement('div'),
-                empty = cartWrapper.querySelector('.empty');
-
-            trigger.remove();
-
-            showConfirm();
-
-            removeBtn.classList.add('goods__item-remove');
-            removeBtn.innerHTML = '&times';
-            item.appendChild(removeBtn);
-
-            cartWrapper.appendChild(item);
-
-            calcGoods();
-            calcTotal();
-            rmFromCart();
-
+        callback();
+    };
+    function createElement(arr) {
+        const goodsWrapper = document.querySelector('.goods__wrapper');
+		
+        arr.forEach(function (item) {
+            let card = document.createElement('div');
+            card.classList.add('goods__item');
+            card.innerHTML = `
+                    <img class="goods__img" src="${item.url}" alt="phone">
+                    <div class="goods__colors">Доступно цветов: 4</div>
+                    <div class="goods__title">
+                        ${item.title}
+                    </div>
+                    <div class="goods__price">
+                        <span>${item.price}</span> руб/шт
+                    </div>
+                    <button class="goods__btn">Добавить в корзину</button>
+               `;
+            goodsWrapper.appendChild(card);
         });
+    }
 
-    });
-            //сокращаем описание товаров до 70 символов
-            function sliceTitle() {
-                titles.forEach(function (item) {
-                    if(item.textContent.length < 70) {
-                        return;
-                    } else {
-                        const str = item.textContent.slice(0 , 71) + '...';
-                        item.textContent = str;
-                    }
-                });
-            }
+    loadContent('js/goods', () => {
+        //выбирая селекторов, объявляем констант
+        const cartWrapper = document.querySelector('.cart__wrapper'),
+            cart = document.querySelector('.cart'),
+            close = document.querySelector('.cart__close'),
+            open = document.querySelector('#cart'),
+            goodsBtn = document.querySelectorAll('.goods__btn'),
+            products = document.querySelectorAll('.goods__item'),
+            confirm = document.querySelector('.confirm'),
+            badge = document.querySelector('.nav__badge'),
+            totalCost = document.querySelector('.cart__total > span'),
+            titles = document.querySelectorAll('.goods__title');
 
-            sliceTitle();
+        // создаем функцию для открытия корзины
+        function openCart() {
+            cart.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }
 
-            //анимация добавление товара на корзину
-            function showConfirm() {
-                confirm.style.display = 'block';
-                let counter = 100;
-                const id = setInterval(frame, 10);
+        //это для закрытия корзину :)
+        function closeCart() {
+            cart.style.display = 'none';
+            document.body.style.overflow = '';
+        }
 
-                function frame() {
-                    if (counter == 10) {
-                        clearInterval(id);
-                        confirm.style.display = 'none';
-                    } else {
-                        counter--;
-                        confirm.style.opacity = '.' + counter;
-                        confirm.style.transform = `translateY(-${counter}px)`;
-                    }
-                }
-            }
+        //чтобы работало функций, назначаем обработчики событий
+        open.addEventListener('click', openCart);
+        close.addEventListener('click', closeCart);
 
-            //считаем товары
-            function calcGoods(i) {
-                const items = cartWrapper.querySelectorAll('.goods__item');
-                      empty = cartWrapper.querySelector('.empty');
-                badge.textContent = items.length;
-                if(items.length == 0) {
-                    empty.style.display = 'block';
+        //создаем массив кнопки "Добавить в корзину" для всех товаров
+        goodsBtn.forEach(function (btn, i) {
+            btn.addEventListener('click', () => {
+                let item = products[i].cloneNode(true),
+                    trigger = item.querySelector('button'),
+                    removeBtn = document.createElement('div'),
+                    empty = cartWrapper.querySelector('.empty');
+
+                trigger.remove();
+				showConfirm();
+
+                removeBtn.classList.add('goods__item-remove');
+                removeBtn.innerHTML = '&times';
+                item.appendChild(removeBtn);
+
+                cartWrapper.appendChild(item);
+
+                calcGoods();
+                calcTotal();
+                rmFromCart();
+            });
+        });
+		
+        //сокращаем описание товаров до 70 символов
+        function sliceTitle() {
+            titles.forEach(function (item) {
+                if(item.textContent.length < 70) {
+                    return;
                 } else {
-                    empty.style.display = 'none';
+                    const str = item.textContent.slice(0 , 71) + '...';
+                    item.textContent = str;
+                }
+            });
+        }
+		
+        sliceTitle();
+
+        //анимация добавление товара на корзину
+        function showConfirm() {
+            confirm.style.display = 'block';
+            let counter = 100;
+            const id = setInterval(frame, 10);
+
+            function frame() {
+                if (counter == 10) {
+                    clearInterval(id);
+                    confirm.style.display = 'none';
+                } else {
+                    counter--;
+                    confirm.style.opacity = '.' + counter;
+                    confirm.style.transform = `translateY(-${counter}px)`;
                 }
             }
+        }
 
-            //считает цены всех товаров
-            function calcTotal() {
-                const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
-                let total = 0;
-                prices.forEach(function (item) {
-                   total += +item.textContent;
-                });
-                totalCost.textContent = total;
+        //считаем товары
+        function calcGoods(i) {
+            const items = cartWrapper.querySelectorAll('.goods__item');
+            empty = cartWrapper.querySelector('.empty');
+            badge.textContent = items.length;
+            if(items.length == 0) {
+                empty.style.display = 'block';
+            } else {
+                empty.style.display = 'none';
             }
+        }
 
-            //функция удаление товара ранее созданный кнопкой
-            function rmFromCart() {
-                const  rmFromCart = cartWrapper.querySelectorAll('.goods__item-remove');
-                rmFromCart.forEach(function (btn) {
-                    btn.addEventListener('click', () => {
-                        btn.parentElement.remove();
-                        calcGoods();
-                        calcTotal();
-                    });
+        //считает цены всех товаров
+        function calcTotal() {
+            const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+            let total = 0;
+            prices.forEach(function (item) {
+                total += +item.textContent;
+            });
+            totalCost.textContent = total;
+        }
+		
+        //функция удаление товара ранее созданный кнопкой
+        function rmFromCart() {
+            const  rmFromCart = cartWrapper.querySelectorAll('.goods__item-remove');
+            rmFromCart.forEach(function (btn) {
+                btn.addEventListener('click', () => {
+                    btn.parentElement.remove();
+                    calcGoods();
+                    calcTotal();
                 });
-            }
-
-
+            });
+        }
+    });
             /*P.S Eсли что-то неправильно напишите мне на почту
               mail: muhanbetkali@gmail.com
               */
-
-        });
-
-
-
+});
